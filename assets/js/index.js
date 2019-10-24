493671fd99b000b49a031164c30e6969ed05e939
AS <albrecht.stephanie@outlook.fr>
2019-10-24 11:51:36 +0200

    correction

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
 var lastName = "Doe";
 var firstName = "John";
 var city = "New-York";
-alert(`Nom : ${lastName},\nPrénom : ${firstName},\nVille : ${city}`);
+alert(`Nom : ${lastName}
+  Prénom : ${firstName}
+  Ville : ${city}`);
+/*alert('Nom : ' + lastName + '\nPrénom : ' + firstName + '\nVille : '+ city');*/
